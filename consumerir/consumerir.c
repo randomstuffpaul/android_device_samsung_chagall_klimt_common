982270d6c496fca5a8bf342b8ee2330815813d66
Joseph Annareddy <josephannareddy@gmail.com>
2016-10-02 06:31:48 -0700

    consumerir: Potentially fix reversed HAL source
    * These are the IR freq ranges chagall and klimt need
    * This still likely is broken without LL kernel IR drivers:
      https://github.com/Schischu/android_kernel_samsung_chagalllte/pull/2
    
    Signed-off-by: Joseph Annareddy <josephannareddy@gmail.com>

--- consumerir/consumerir.c
+++ consumerir/consumerir.c
@@ -38,7 +38,16 @@
 #define IR_BUFFER_SIZE 0x1000
 
 static const consumerir_freq_range_t consumerir_freqs[] = {
+#ifdef USE_ONE_FREQ_RANGE
     {.min = 16000, .max = 60000},
+#else
+    {.min = 30000, .max = 30000},
+    {.min = 33000, .max = 33000},
+    {.min = 36000, .max = 36000},
+    {.min = 38000, .max = 38000},
+    {.min = 40000, .max = 40000},
+    {.min = 56000, .max = 56000},
+#endif
 };
 
 static char* SEPARATOR_EOF = "\0";
